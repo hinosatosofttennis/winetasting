fd1abd34684de0804c4753bb4045047e9bdaa32e
日の里ソフトテニス倶楽部 <hinosatosofttennis@gmail.com>
2025-09-28 21:22:26 +0900

    Create firebaseConfig.js
    New

---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Firebase Consoleから取得した設定値に置き換えてください
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// 必要なモジュールをインポートし、初期化
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
